36b41c6299db1dfb2e2a15b47982983a1f983249
IbrahimSquared <ibrahim.ibrahim@kuleuven.be>
2024-09-05 09:53:21 +0200

    default benchmark, add evaluate query example

--- src/main.cpp
+++ src/main.cpp
@@ -9,13 +9,20 @@ int main() {
     std::cerr << "Error parsing config file" << std::endl;
     return 1;
   }
-  config_parser.printConfig();
+  // config_parser.printConfig();
 
   accelerated::Solver solver;
   // solver.RandomPathsValidityChecks();
-  solver.drawRandomPath();
-  // solver.benchmarkPlanners();
+  // solver.drawRandomPath();
+  solver.benchmarkPlanners();
   // solver.characterizeVariability();
+
+  // const double pi = 3.1415926535897932;
+  // // query a single path
+  // accelerated::State from = {100, 100, 0};
+  // accelerated::State to = {101.24796, 101.64845, -0.9306};
+  // const double radius = 1;
+  // solver.evaluateQuery(from, to, radius);
 
   return 0;
 }
